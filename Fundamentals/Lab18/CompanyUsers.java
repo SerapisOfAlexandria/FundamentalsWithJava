842bf6a274ad5848ab165e3334bbf0b4b77b69e2
Stan <slavi.nedqlkov@abv.bg>
2023-11-10 12:15:40 -0800

    Finished Task "Company Users"

---
+++ Fundamentals/Lab18/CompanyUsers.java
@@ -0,0 +1,37 @@
+package Lab18;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class CompanyUsers {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        LinkedHashMap<String, ArrayList<String>> employeesInCompanies = new LinkedHashMap<>();
+        String[] companyAndEmploye = input.nextLine().split(" -> ");
+
+        while (!companyAndEmploye[0].equals("End")) {
+            String company = companyAndEmploye[0];
+            String employee = companyAndEmploye[1];
+
+            if (employeesInCompanies.containsKey(company)) {
+                employeesInCompanies.get(company).add(employee);
+            } else {
+                employeesInCompanies.put(company, new ArrayList<>(Arrays.asList(employee)));
+            }
+
+            companyAndEmploye = input.nextLine().split(" -> ");
+        }
+
+        employeesInCompanies.entrySet().stream()
+                .map(entry -> entry.getKey() + "\n" +
+                        entry.getValue().stream()
+                                .map(empl -> "-- " + empl)
+                                .collect(Collectors.joining("\n")))
+                .forEach(System.out::println);
+    }
+}
