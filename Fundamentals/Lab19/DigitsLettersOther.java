149346045ce52db9aa927b56202924f3f8a4958e
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-11-13 22:53:39 +0300

    Finished Task "Digits, letters, and others"

---
+++ Fundamentals/Lab19/DigitsLettersOther.java
@@ -0,0 +1,33 @@
+package Fundamentals.Lab19;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class DigitsLettersOther {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        List<Character> chars = input.nextLine().chars()
+                .mapToObj(n -> (char) n)
+                .collect(Collectors.toList());
+
+        System.out.println(chars.stream()
+                .filter(n -> Character.isDigit(n))
+                .map(String::valueOf)
+                .collect(Collectors.joining("")));
+
+        System.out.println(chars.stream()
+                .filter(n -> Character.isLetter(n))
+                .map(String::valueOf)
+                .collect(Collectors.joining("")));
+
+        System.out.println(chars.stream()
+                .filter(n -> !Character.isLetterOrDigit(n))
+                .map(String::valueOf)
+                .collect(Collectors.joining("")));
+    }
+}
