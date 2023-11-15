9156ba19fa313f8b4d57a7c183bb5467e79dd25f
Stan <slavi.nedqlkov@abv.bg>
2023-11-15 17:37:35 +0200

    Fixed a bug in task "String Explosion"

---
+++ Fundamentals/Lab20/StringExplosion.java
@@ -0,0 +1,30 @@
+package Lab20;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class StringExplosion {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Character> string = new ArrayList<>();
+        char[] charredString = input.nextLine().toCharArray();
+        StringBuilder finishedString = new StringBuilder();
+
+        for (char a : charredString) {
+            string.add(a);
+        }
+        int pointer = 0;
+
+        while (pointer <= string.size() - 1) {
+
+            if (string.get(pointer) == '>') {
+                for (int i = pointer; i <= pointer + )
+            }
+        }
+    }
+
+    public static void explode(int pointer, ArrayList<String> symbolList) {
+
+    }
+}
